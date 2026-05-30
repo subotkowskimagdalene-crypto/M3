93d68069e06bcc75c4e19400fb00850d152dd2a5
subotkowskimagdalene-crypto <subotkowskimagdalene@nhsdlions.org>
2026-05-30 19:52:29 -0400

    Add files via upload

---
+++ my_code_effects.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   $("p").hide();
+   
+   $("h1").click(function(){
+     $("p").toggle(1000); 
+   });
+
+
+
+ $(".toggle").click(function(){
+     $("p").toggle(1000); 
+   });
+
+});
